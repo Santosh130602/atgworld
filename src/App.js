86c5741bcaaa6baffb115159bf6d73a7866850e9
Santosh130602 <159407236+Santosh130602@users.noreply.github.com>
2024-06-28 22:49:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login'
 import Signup from './components/SignUp'
@@ -6,16 +6,18 @@ import Posts from './components/Posts';
 import Navbar from './components/Navbar';
 
 
+
 function App() {
   return (
     <>
-      
+      <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/posts' element={<Posts />} />
       </Routes>
+     </BrowserRouter>
     </>
   );
 }
