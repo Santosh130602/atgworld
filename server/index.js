01a63ef9601206d9c2750ea1b4ec512448ecee43
Santosh130602 <159407236+Santosh130602@users.noreply.github.com>
2024-06-28 22:36:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -45,27 +45,30 @@
 
 
 
-
 const express = require('express');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
-const cors = require("cors");
+const cors = require('cors');
 const postRoutes = require('./routes/postRoutes');
 const commentRoutes = require('./routes/commentRoutes');
 const { errorHandler } = require('./middleware/errorMiddleware');
-
-const colors = require("colors");
+const colors = require('colors');
 const database = require('./config/db');
-
-
-
 
 dotenv.config();
 const app = express();
- 
-app.use(cors({
-    origin: ['https://atgworld-chi.vercel.app'],
-}));
+
+const corsOptions = {
+    origin: 'https://atgworld-chi.vercel.app',
+    optionsSuccessStatus: 200,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    allowedHeaders: 'Content-Type,Authorization'
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Enable pre-flight
+
 app.use(express.json());
 
 app.use('/api/user', userRoutes);
@@ -77,16 +80,11 @@ app.use(errorHandler);
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
+
 database.connect();
 
 const PORT = process.env.PORT || 5000;
 
-
-
 app.listen(PORT, () => {
-    console.log(`Server started on PORT ${PORT}`.green.bold)
+    console.log(`Server started on PORT ${PORT}`.green.bold);
 });
-
-
-
-
