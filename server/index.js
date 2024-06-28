e50a0248bf5df2318c8e6fc03e2f1af69c276f82
Santosh130602 <159407236+Santosh130602@users.noreply.github.com>
2024-06-28 21:20:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,49 @@
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const userRoutes = require('./routes/userRoutes');
+// const cors = require("cors");
+// const postRoutes = require('./routes/postRoutes');
+// const commentRoutes = require('./routes/commentRoutes');
+// const { errorHandler } = require('./middleware/errorMiddleware');
+
+// const colors = require("colors");
+// const database = require('./config/db');
+
+// dotenv.config();
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+
+
+
+
+
+
+
+// app.use('/api/user', userRoutes);
+// app.use('/api/posts', postRoutes);
+// app.use('/api/comments', commentRoutes);
+
+// app.use(errorHandler);
+
+// app.get('/', (req, res) => {
+//     res.send('Server is running');
+// });
+// database.connect();
+
+// const PORT = process.env.PORT || 5000;
+
+
+
+// app.listen(PORT, () => {
+//     console.log(`Server started on PORT ${PORT}`.green.bold)
+// });
+
+
+
+
 const express = require('express');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
@@ -9,17 +55,15 @@ const { errorHandler } = require('./middleware/errorMiddleware');
 const colors = require("colors");
 const database = require('./config/db');
 
+
+app.use(express.static('public'));
+
 dotenv.config();
 const app = express();
-
-app.use(express.json());
-app.use(cors());
-
-
-
-
-
-
+ 
+app.use(cors({
+    origin: 'https://atgworld-16rnyxr8d-santosh-s-projects-b83e3098.vercel.app'
+}));
 
 
 app.use('/api/user', userRoutes);
@@ -40,4 +84,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server started on PORT ${PORT}`.green.bold)
 });
+
 
