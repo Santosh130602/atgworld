4ef3928c291f45c014c564ec76d965e260a838ef
Santosh130602 <159407236+Santosh130602@users.noreply.github.com>
2024-06-28 21:55:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -49,6 +49,56 @@
 
 
 
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const cors = require('cors');
+// const colors = require('colors');
+// const database = require('./config/db');
+// const userRoutes = require('./routes/userRoutes');
+// const postRoutes = require('./routes/postRoutes');
+// const commentRoutes = require('./routes/commentRoutes');
+// const { errorHandler } = require('./middleware/errorMiddleware');
+
+// dotenv.config(); // Load environment variables
+
+// const app = express();
+
+// const corsOptions = {
+//     origin: 'https://atgworld-chi.vercel.app', // replace with your Vercel domain
+//     optionsSuccessStatus: 200,
+// };
+
+// // Use CORS middleware before defining routes
+// app.use(cors(corsOptions));
+// app.use(express.json()); // Middleware to parse JSON bodies
+
+// app.use(express.static('public')); // Serve static files
+
+// // API Routes
+// app.use('/api/user', userRoutes);
+// app.use('/api/posts', postRoutes);
+// app.use('/api/comments', commentRoutes);
+
+// // Error handling middleware
+// app.use(errorHandler);
+
+// // Root route
+// app.get('/', (req, res) => {
+//     res.send('Server is running');
+// });
+
+// // Connect to the database
+// database.connect();
+
+// // Start the server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server started on PORT ${PORT}`.green.bold);
+// });
+
+
+
+
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
@@ -70,9 +120,12 @@ const corsOptions = {
 
 // Use CORS middleware before defining routes
 app.use(cors(corsOptions));
-app.use(express.json()); // Middleware to parse JSON bodies
 
-app.use(express.static('public')); // Serve static files
+// Middleware to parse JSON bodies
+app.use(express.json()); 
+
+// Serve static files
+app.use(express.static('public')); 
 
 // API Routes
 app.use('/api/user', userRoutes);
