c637751b9a826b70ee89799cd82211f4a9a5ef11
Santosh130602 <159407236+Santosh130602@users.noreply.github.com>
2024-06-28 22:54:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,12 +8,9 @@ import { ChakraProvider } from '@chakra-ui/react'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
       <ChakraProvider>
         <App />
       </ChakraProvider>
-    </Router>
-
   </React.StrictMode>
 );
 
